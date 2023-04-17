78a8a52562aa9fd6f69325d19289133d18d83c90
Rajeshwarikota <kotarajeswari003.com>
2023-04-17 15:26:58 +0530

    [Rajeshwari kota] Creating customer table

--- Assignment.sql
+++ Assignment.sql
@@ -1,4 +1,11 @@
 -----------Assignment Details-----------------
-----------UC1-Create Customer Table------------
+----------UC1-CREATE DATABSE------------------
 Create Database Assignment;
 Use Assignment;
+----------UC2-CREATE CUSTOMERTABLE------------
+CREATE  TABLE Customer (
+    Id int primary key identity(1,1),
+    Name varchar(100),
+    PhoneNumber varchar(15),
+    Address varchar(200)
+   );
