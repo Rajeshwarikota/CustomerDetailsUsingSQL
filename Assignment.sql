05f8e4cf250c78e32a42d59183eebb6aa36e322e
Rajeshwarikota <kotarajeswari003.com>
2023-04-17 15:34:41 +0530

    [Rajeshwari kota] Adding Customer tables into table

--- Assignment.sql
+++ Assignment.sql
@@ -9,3 +9,10 @@ CREATE  TABLE Customer (
     PhoneNumber varchar(15),
     Address varchar(200)
    );
+---------UC3-ADDING CUSTOMER DETAILS----------
+insert into Customer values ('Raji',9988776655,'Hyderabad')
+insert into Customer values ('Teju',9966558877,'Chennai')
+insert into Customer values ('Chandu',9988775522,'Bangalore')
+insert into Customer values ('ammu',9955884422,'mumbai')
+
+select * from Customer
